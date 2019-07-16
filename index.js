3b5676d767a343c4bbdb204459bc8380822c8147
Ogunboyejo Olatunde <tunde.ogunboyejo@xputertechnologies.com>
2019-07-16 19:30:36 +0100

    added post build script

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path')
 const { mongoDbURI } = require('./config/keys');
 
 const app = express();
@@ -24,6 +25,14 @@ db.once('open', () => {
   require('./routes/users')(app);
   require('./routes/auth')(app);
   require('./routes/contacts')(app);
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+  }
   console.log(`Server running on port ${PORT} 🔥`);
 
 })
